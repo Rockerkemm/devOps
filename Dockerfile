b386577be0906230a8d4bb16733854119f1b136b
Daniel <dsmyth@outlook.com>
2025-10-17 11:54:02 +0100

    made flask run through docker

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.14-slim
+
+WORKDIR /app
+
+RUN apt-get update && apt-get install -y \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN pip install --no-cache-dir flask
+
+COPY hello.py .
+
+CMD ["flask", "--app", "hello", "run", "--host=0.0.0.0"]
--- hello.py
+++ hello.py
@@ -4,13 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-	return '<p>This is another string!</p> \n<p>Welcome, World, I am a Flask app!</p>'
+    return '<p>This is another string!</p> \n<p>Welcome, World, I am a Flask app!</p>'
 
 @app.route('/about')
 def about():
-	return '<a href="https://flask.palletsprojects.com/en/stable/">Flask Documentation</a>\n' 	'<p>This is a simple application running on flask.</p>'
-	       '<a href="https://www.python.org">Python Web</a>'
+    return '<a href="https://flask.palletsprojects.com/en/stable/">Flask Documentation</a>\n' \
+           '<p>This is a simple application running on flask.</p>' \
+           '<a href="https://www.python.org">Python Web</a>'
 
-@app.route('/about')
-def about():
-	return '<a href="C23387203@mytudublin.ie">C23387203@mytudublin.ie</a>\n' 	'<p>Student Email</p>'
+@app.route('/contact')
+def contact():
+    return '<a href="mailto:C23387203@mytudublin.ie">C23387203@mytudublin.ie</a>\n' \
+           '<p>Student Email</p>'
