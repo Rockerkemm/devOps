aeaf8180fd7db7a229034512deae02979617c56e
Daniel <dsmyth@outlook.com>
2025-10-26 13:16:55 +0000

    redis and docker compose

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN apt-get update && apt-get install -y \
     git \
     && rm -rf /var/lib/apt/lists/*
 
-RUN pip install --no-cache-dir flask
+RUN pip install --no-cache-dir flask redis
 
 EXPOSE 5000
 
--- hello.py
+++ hello.py
@@ -1,18 +1,59 @@
 from flask import Flask
+import redis
+import json
+import time
 
 app = Flask(__name__)
 
+# Connect to Redis server
+r = redis.Redis(host='redis-server', port=6379, decode_responses=True)
+
 @app.route('/')
 def say_hello():
-    return '<p>This is another string!</p> \n<p>Welcome, World, I am a Flask app!</p>'
+    data = r.get('home')
+    if data is not None:
+        data = json.loads(data)
+        # 600 seconds == 10 minutes
+        if time.time() - data['time'] <= 600:
+            return data['html']
+    
+    html_content = '<p>This is another string!</p> \n<p>Welcome, World, I am a Flask app!</p>'
+    r.set('home', json.dumps({
+        'html': html_content,
+        'time': time.time()
+    }))
+    return html_content
 
 @app.route('/about')
 def about():
-    return '<a href="https://flask.palletsprojects.com/en/stable/">Flask Documentation</a>\n' \
+    data = r.get('about')
+    if data is not None:
+        data = json.loads(data)
+        # 600 seconds == 10 minutes
+        if time.time() - data['time'] <= 600:
+            return data['html']
+    
+    html_content = '<a href="https://flask.palletsprojects.com/en/stable/">Flask Documentation</a>\n' \
            '<p>This is a simple application running on flask.</p>' \
            '<a href="https://www.python.org">Python Web</a>'
+    r.set('about', json.dumps({
+        'html': html_content,
+        'time': time.time()
+    }))
+    return html_content
 
 @app.route('/contact')
 def contact():
-    return '<a href="mailto:C23387203@mytudublin.ie">C23387203@mytudublin.ie</a>\n' \
+    data = r.get('contact')
+    if data is not None:
+        data = json.loads(data)
+        if time.time() - data['time'] <= 600:
+            return data['html']
+    
+    html_content = '<a href="mailto:C23387203@mytudublin.ie">C23387203@mytudublin.ie</a>\n' \
            '<p>Student Email</p>'
+    r.set('contact', json.dumps({
+        'html': html_content,
+        'time': time.time()
+    }))
+    return html_content
